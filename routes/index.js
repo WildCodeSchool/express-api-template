4061a9a1b959d18adc9ab2e04588353a6bf0fad2
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-12-14 14:43:06 +0100

    tmp

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const contactRoutes = require('./contacts');
 const userRoutes = require('./users');
+const authRoutes = require('./auth');
 
 module.exports = (app) => {
   app.use('/contacts', contactRoutes);
   app.use('/users', userRoutes);
+  app.use('/auth', authRoutes);
 };
